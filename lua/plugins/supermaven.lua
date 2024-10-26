8dd4c45c0eb608e27b602544b8feddf69a0a8b52
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-10-26 03:57:09 +0200

    add supermaven-nvim

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({})
+		end,
+		opts = {
+			keymaps = {
+				accept_suggestion = "<Tab>",
+				clear_suggestion = "<S-Tab>",
+				accept_word = "<C-j>",
+			},
+			--ignore_filetypes = { "cpp" = true },
+			color = {
+				suggestion_color = "#ffffff",
+				cterm = 244,
+			},
+			log_level = "info", -- "off" to disable logging
+			disable_inline_completion = false, --for use with cmp
+			disable_keymaps = false, -- disable built-in keymaps for more manual control
+			condition = function()
+				return false
+			end -- condition to disable the plugin
+
+		},
+	}
+}
+
